c9f4b0e3f4494e39d8f7eedcdbc16f86b51d9be2
long-2005 <concathu119@gmail.com>
2024-11-10 11:37:19 +0700

    Update train.py

--- FeaturesFusionRGBDepth-main/train_sh/train.py
+++ FeaturesFusionRGBDepth-main/train_sh/train.py
@@ -170,6 +170,9 @@ def run(
     
     lr = init_lr
     optimizer = optim.AdamW(model.parameters(), lr=lr,weight_decay=0.01)
+    tep_size = 5  # Sau mỗi 10 epochs sẽ giảm lr
+    gamma = 0.1  # Giảm lr xuống 10% mỗi lần
+    scheduler = StepLR(optimizer, step_size=step_size, gamma=gamma)        
    
 # Giảm LR khi validation loss không cải thiện sau 5 epochs
 
